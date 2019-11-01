6228cb59e67595dce570235b3b9c56b0179b7871
lawrenceagles <lawrenceagles@gmail.com>
2019-11-01 11:33:48 +0100

    login form validation done 100%

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
-const Login = () => {
 
-    const renderInput = ({ input, label, meta, fieldType }) => {
+const renderInput = ({ input, label, meta:{ touched, error }, type, placeholder }) => { // render input fields
 
-        return (
-            <div className="form-group">
-                <label>{ label }</label>
-                <input { ...input } type={ fieldType } className="form-control"  />
-            </div>
-        )
+    return (
+        <div className="form-group">
+            <label>{ label }</label>
+            <input { ...input } className="form-control" type={ type } placeholder={ placeholder }  />
+            <div className="text-danger">{ touched && error && <small>{ error }</small> }</div>
+        </div>
+    )
+}
+
+const Login = ({ handleSubmit }) => {
+
+    const submitForm = (formValues) => {
+        console.log("form values", formValues)
     }
     
     return (
@@ -19,12 +25,12 @@ const Login = () => {
 
                 <article className="col-10 mx-auto">
                     <h1 className="text-center py-4">Welcome Login</h1>
-                    <form>
-                        <Field name="identifier" component={ renderInput } label="Enter Username" fieldType={ "text" } />
-                        <Field name="password" component={ renderInput } label="Enter password" fieldType={ "password" } />
+                    <form onSubmit={ handleSubmit(submitForm) }>
+                        <Field name="identifier" component={ renderInput } label="Enter Username" type="text" placeholder="Enter your username" />
+                        <Field name="password" component={ renderInput } label="Enter password" type="password" placeholder="Enter Your Password" />
                         
                         <div className="col-6 mx-auto py-2">
-                            <button type="button" className="btn btn-success btn-block"><strong>Login</strong></button>
+                            <button type="submit" className="btn btn-success btn-block"><strong>Login</strong></button>
                         </div>
                     </form>
                 </article>
@@ -35,8 +41,23 @@ const Login = () => {
 }
 
 
+const validate = (formValues) => {// validate form fields
+    const errors = {}
+    
+    if(!formValues.identifier){
+        errors.identifier = "Please enter a username"
+    }
+
+    if(!formValues.password){
+        errors.password = "Please enter a password"
+    }
+
+    console.log("validating", errors)
+    return errors
+}
 
 
 export default reduxForm({
-    form: 'loginForm'
-  })(Login);
+    form: 'loginForm',
+    validate
+  })(Login)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import registrationReducer from './registrationReducer'
 import loginReducer from './loginReducer'
 
 
 export default combineReducers({
     registeration: registrationReducer,
-    login: loginReducer
+    login: loginReducer,
+    form: formReducer
 })
